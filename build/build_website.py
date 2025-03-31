d26e64013aacc9eff5133071db803a812b7da1dc
Pieter Pierrot <p.j.pierrot@uva.nl>
2025-03-31 16:21:04 +0200

    update slide buttons

--- build/build_website.py
+++ build/build_website.py
@@ -36,7 +36,7 @@ def _create_document_list(document_dict):
 
 			doc_html = doc_html.replace("<!--$$NAME$$-->", "")
 			# d["name"] + (" (link TBA)" if len(d["link"])==0 else "")
-			doc_html = doc_html.replace("<!--$$LINK$$-->", f"href='{slide_link}'" if slide_link else "")
+			doc_html = doc_html.replace("<!--$$LINK$$-->", f"href='{slide_html}'" if slide_link else "")
 			# ("href='%s'"%d["link"]) if len(d["link"])>0 else ""
 			doc_html = doc_html.replace("<!--$$ICON$$-->", ICONS.get(d["type"], DEFAULT_ICON))
 
